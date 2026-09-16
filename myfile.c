12fa34d6b1f6dcfcb345b1e007947b5622f343f9
Bastain Slater-Tyler <bastiantyler32@gmail.com>
2026-09-16 10:32:45 -0700

    Init Commit

---
+++ myfile.c
@@ -0,0 +1,26 @@
+/**
+ * File Name: <"myfile.c">
+ * Owner: <"bslat">
+ * Last Modified On: <"Sep"> <"8"> <"23:25">
+ */
+/**
+ * File Name: <"myfile.c">
+ * Owner: <"bslat">
+ * Last Modified On: <"Sep"> <"8"> <"23:25">
+ */
+/**
+ * File Name: <"myfile.c">
+ * Owner: <"bslat">
+ * Last Modified On: <"Sep"> <"8"> <"23:24">
+ */
+/**
+ * File Name: <"myfile.c">
+ * Owner: <"bslat">
+ * Last Modified On: <"Sep"> <"8"> <"23:22">
+ */
+int main(void)
+{
+	printf("Hello!\n");
+
+	return 0;
+}
