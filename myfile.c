4f037235ea42e2430b9cb15a7f5ee2a6e467f003
Bat Daven Slater-Tyler <bslatert@linux1.csc.calpoly.edu>
2026-09-16 14:15:43 -0700

    Fixed the lab for the server

--- myfile.c
+++ myfile.c
@@ -1,5 +1,30 @@
 /**
  * File Name: <"myfile.c">
+ * Owner: <"bslatert">
+ * Last Modified On: <"Sep"> <"16"> <"14:13">
+ */
+/**
+ * File Name: <"myfile.c">
+ * Owner: <"domain">
+ * Last Modified On: <"Sep"> <"16"> <"14:12">
+ */
+/**
+ * File Name: <"14:11">
+ * Owner: <"bslatert">
+ * Last Modified On: <"Sep"> <"16"> <"14:11">
+ */
+/**
+ * File Name: <"14:06">
+ * Owner: <"bslatert">
+ * Last Modified On: <"Sep"> <""> <"14:06">
+ */
+/**
+ * File Name: <"14:05">
+ * Owner: <"bslatert">
+ * Last Modified On: <"452"> <"Sep"> <"16">
+ */
+/**
+ * File Name: <"myfile.c">
  * Owner: <"bslat">
  * Last Modified On: <"Sep"> <"8"> <"23:25">
  */
